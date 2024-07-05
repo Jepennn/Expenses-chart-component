fa5e01a10e9f83619c4d82ebee9a5b1c1fe370a8
Jepennn <jeppehesselgren@gmail.com>
2024-07-05 15:58:05 +0200

    Finshed desgin, layout and js

--- script.js
+++ script.js
@@ -6,6 +6,14 @@
     // Get the days of the week
     const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
+    //Get all paragrafs elements
+    const amountParagrafs = document.querySelectorAll('.amount');
+
+    // Update the amounts
+    amountParagrafs.forEach((paragraf, index) => {
+      paragraf.textContent = `$${data[index].amount}`;
+    });
+
     // Initialize totals object and sum
     const totals = {};
     let sum = 0;
@@ -13,23 +21,40 @@
     // Initialize totals object
     days.forEach(day => totals[day] = 0);
 
-    // Sum up the amounts for each day
+    // Summerize the amounts for each day
     data.forEach(item => {
       if (days.includes(item.day)) {
         totals[item.day] += item.amount;
         sum += item.amount;
       } 
     }); 
-    
-    // Update the bars
+
+    //Update the bars
     days.forEach(day => {
       const bar = document.getElementById(`${day}`);
       bar.style.height = `${totals[day]}px`;
+
     });
 
-    // Update the total amount
-    document.getElementById('total-amount').textContent = sum;
-
+    //Update the total amount
+    document.getElementById('total-amount').textContent = `$${sum.toFixed(2)}`;
 
   });
-  
+
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    // Först, få referenser till elementen
+    var bars = document.querySelectorAll('.bars');
+    var lable = document.querySelectorAll('.lable');
+
+    // Sedan, lägg till händelsehanterare för varje bar-element
+    bars.forEach((bar, index) => {
+        bar.addEventListener('mouseover', () => {
+            lable[index].style.opacity = '1';
+        });
+
+        bar.addEventListener('mouseout', () => {
+            lable[index].style.opacity = '0';
+        });
+    });
+});
